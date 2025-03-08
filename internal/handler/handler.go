e79def87fd12b7d503602938516b5be7c0af0515
Emre Demirel <emredemirel2020@yandex.ru>
2025-03-08 17:55:56 +0300

    feat: add new allowed header

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -45,7 +45,7 @@ func CorsMiddleware(next func (http.ResponseWriter, *http.Request), cfg configs.
 			} else {
 				w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
 				w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token")
 				w.Header().Set("Access-Control-Allow-Credentials", "true")
 				w.WriteHeader(http.StatusForbidden)
 				return
@@ -55,7 +55,7 @@ func CorsMiddleware(next func (http.ResponseWriter, *http.Request), cfg configs.
 		}
 
         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token")
         w.Header().Set("Access-Control-Allow-Credentials", "true")
 
         next(w, r)
