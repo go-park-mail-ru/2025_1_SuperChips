dff530bd148f4e3d1fadc2957744731f93bfccc8
Emre Demirel <emredemirel2020@yandex.ru>
2025-03-08 18:05:30 +0300

    feat: allow all method in cors

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -44,7 +44,7 @@ func CorsMiddleware(next func (http.ResponseWriter, *http.Request), cfg configs.
 				w.Header().Set("Access-Control-Allow-Origin", origin)
 			} else {
 				w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-				w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+				w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
 				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token")
 				w.Header().Set("Access-Control-Allow-Credentials", "true")
 				w.WriteHeader(http.StatusForbidden)
@@ -54,7 +54,7 @@ func CorsMiddleware(next func (http.ResponseWriter, *http.Request), cfg configs.
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 		}
 
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token")
         w.Header().Set("Access-Control-Allow-Credentials", "true")
 
