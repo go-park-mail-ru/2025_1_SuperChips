2d711c7e8bf939936f0195dfd02813574044eb1e
Emre Demirel <emredemirel2020@yandex.ru>
2025-03-23 21:10:45 +0300

    feat: add new pgHost env variable to pg config

--- app/main.go
+++ app/main.go
@@ -34,7 +34,7 @@ func main() {
 		log.Fatalf("Cannot launch due to pg config error: %s", err)
 	}
 
-	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", "localhost", 5432, pgConfig.PgUser, pgConfig.PgPassword, pgConfig.PgDB)
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", pgConfig.PgHost, 5432, pgConfig.PgUser, pgConfig.PgPassword, pgConfig.PgDB)
 	db, err := pg.ConnectDB(psqlconn)
 	if err != nil {
 		log.Fatalf("Cannot launch due to database connection error: %s", err)
--- configs/pg.go
+++ configs/pg.go
@@ -9,6 +9,7 @@ type PostgresConfig struct {
 	PgUser     string // имя пользователя для входа
 	PgPassword string // пароль
 	PgDB       string // имя бдшки
+	PgHost     string // хост
 }
 
 func (config *PostgresConfig) LoadConfigFromEnv() error {
@@ -27,9 +28,15 @@ func (config *PostgresConfig) LoadConfigFromEnv() error {
 		return errors.New("missing environment variable POSTGRES_DB")
 	}
 
+	pgHost, ok := os.LookupEnv("POSTGRES_HOST")
+	if !ok {
+		return errors.New("missing environment variable POSTGRES_HOST")
+	}
+
 	config.PgUser = pgUser
 	config.PgPassword = pgPassword
 	config.PgDB = pgDB
+	config.PgHost = pgHost
 
 	return nil
 }
