03b128eb0c3392331a493ad1d45133dab7bf104c
ForgeUp <sh.383.fank@mail.ru>
2025-04-09 16:41:42 +0300

    PinData: Структура доработана для CRUD пинов

--- domain/feed.go
+++ domain/feed.go
@@ -1,12 +1,13 @@
 package domain
 
 type PinData struct {
-	FlowID      uint64 `json:"-"`
+	FlowID      uint64 `json:"flow_id,omitempty"`
 	Header      string `json:"header,omitempty"`
+	AuthorID    uint64 `json:"author_id"`
 	Description string `json:"description,omitempty"`
 	MediaURL    string `json:"image,omitempty"`
-	AuthorID    uint64 `json:"author_id"`
 	IsPrivate   bool   `json:"-"`
 	Created_at  string `json:"-"`
-	Updated_at  string `json:"-"`
+	IsLiked     bool   `json:"is_liked"`
+	LikeCount   int    `json:"like_count"`
 }
