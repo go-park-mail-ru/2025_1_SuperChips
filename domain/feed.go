8d1f4faa2a25a875d236ef22182c503f2dff0ad8
Emre Demirel <emredemirel2020@yandex.ru>
2025-04-13 04:21:12 +0300

    fix: fixed json tag field

--- domain/feed.go
+++ domain/feed.go
@@ -4,7 +4,7 @@ type PinData struct {
 	FlowID         uint64 `json:"flow_id,omitempty"`
 	Header         string `json:"header,omitempty"`
 	AuthorID       uint64 `json:"author_id,omitempty"`
-	AuthorUsername string `json:"author_username`
+	AuthorUsername string `json:"author_username"`
 	Description    string `json:"description,omitempty"`
 	MediaURL       string `json:"media_url,omitempty"`
 	IsPrivate      bool   `json:"is_private"`
