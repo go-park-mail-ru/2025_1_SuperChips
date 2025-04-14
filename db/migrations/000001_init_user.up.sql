1caf3caf522943bac9c473346bab3520c878fbfe
emresha <emredemirel2020@yandex.ru>
2025-04-14 14:13:05 +0300

    fix: autoincrementation is now done by using identities; added checks to text fields

--- db/migrations/000001_init_user.up.sql
+++ db/migrations/000001_init_user.up.sql
@@ -1,21 +1,14 @@
-CREATE SEQUENCE IF NOT EXISTS flow_user_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
 CREATE TABLE IF NOT EXISTS flow_user (
-    id INTEGER DEFAULT nextval('flow_user_id_seq') PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE,
+    id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE CHECK(LENGTH(username) <= 128),
     avatar TEXT DEFAULT '',
-    public_name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
+    public_name TEXT NOT NULL CHECK(LENGTH(public_name) <= 128),
+    email TEXT NOT NULL UNIQUE CHECK(LENGTH(email) <= 128),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     password TEXT NOT NULL,
     birthday DATE,
-    about TEXT,
+    about TEXT CHECK(LENGTH(about) <= 2048),
     jwt_version INTEGER NOT NULL DEFAULT 1
 );
 
--- db/migrations/000002_init_flow.up.sql
+++ db/migrations/000002_init_flow.up.sql
@@ -1,20 +1,12 @@
-CREATE SEQUENCE IF NOT EXISTS flow_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
 CREATE TABLE IF NOT EXISTS flow (
-    id INTEGER DEFAULT nextval('flow_id_seq') PRIMARY KEY,
-    title TEXT,
-    description TEXT,
+    id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+    title TEXT CHECK (LENGTH(title) <= 128),
+    description TEXT CHECK (LENGTH(description) <= 512),
     author_id INTEGER NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     is_private BOOLEAN NOT NULL DEFAULT FALSE,
-    media_url TEXT NOT NULL,
+    media_url TEXT NOT NULL, -- генерится на бэке, так что мне кажется, здесь ограничение не нужно
     like_count INTEGER NOT NULL DEFAULT 0,
     FOREIGN KEY (author_id) REFERENCES flow_user(id)
 );
