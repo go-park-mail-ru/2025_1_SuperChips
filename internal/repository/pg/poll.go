d59e1753ef933b399d071cddfdd1e8b8ec3c1eb4
ForgeUp <sh.383.fank@mail.ru>
2025-04-26 13:12:57 +0300

    polls: Добавлены базовые структуры

---
+++ internal/repository/pg/poll.go
@@ -0,0 +1,33 @@
+package repositiry
+
+import (
+	"database/sql"
+)
+
+type pollDBSchema struct {
+	ID        uint64
+	Name      sql.NullString
+	CreatedAt sql.NullTime
+}
+
+type questionDBSchema struct {
+	ID        uint64
+	PollID    uint64
+	OrderNum  int64
+	Content   sql.NullString
+	Type      sql.NullString
+	AuthorID  uint64
+	CreatedAt sql.NullTime
+}
+
+type pgPollStorage struct {
+	db *sql.DB
+}
+
+func NewPGPollStorage(db *sql.DB) (*pgPollStorage, error) {
+	storage := &pgPollStorage{
+		db: db,
+	}
+
+	return storage, nil
+}
